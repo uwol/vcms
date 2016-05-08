8bcac9c54e9f9e2d2f4bb6a258dfac5e213dda2c
uwol <u.wol@wwu.de>
2016-05-08 15:55:01 +0200

    panier

--- lib/own/global.class.php
+++ lib/own/global.class.php
@@ -24,7 +24,7 @@ class LibGlobal{
 	var $page;
 	var $iid;
 	var $libInclude;
-	var $version = '4.01';
+	var $version = '4.02';
 	var $errorTexts = array();
 	var $notificationTexts = array();
 	var $vcmsHostname;
