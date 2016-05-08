4e9dc38065270e565d911641768701198b7c1a99
uwol <u.wol@wwu.de>
2016-05-08 11:41:33 +0200

    simplified box model

--- lib/own/global.class.php
+++ lib/own/global.class.php
@@ -24,7 +24,7 @@ class LibGlobal{
 	var $page;
 	var $iid;
 	var $libInclude;
-	var $version = '3.35';
+	var $version = '3.36';
 	var $errorTexts = array();
 	var $notificationTexts = array();
 	var $vcmsHostname;
