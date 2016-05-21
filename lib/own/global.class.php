c62b86e4d93917c4f1867834344b2352a8aa254a
uwol <u.wol@wwu.de>
2016-05-21 23:54:38 +0200

    css fix

--- lib/own/global.class.php
+++ lib/own/global.class.php
@@ -24,7 +24,7 @@ class LibGlobal{
 	var $page;
 	var $iid;
 	var $libInclude;
-	var $version = '4.23';
+	var $version = '4.24';
 	var $errorTexts = array();
 	var $notificationTexts = array();
 	var $vcmsHostname;
