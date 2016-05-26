623c180c465ba199c1bd52a246ed2bc9a3fe445c
uwol <u.wol@wwu.de>
2016-05-26 16:12:07 +0200

    menu color fix

--- lib/own/global.class.php
+++ lib/own/global.class.php
@@ -24,7 +24,7 @@ class LibGlobal{
 	var $page;
 	var $iid;
 	var $libInclude;
-	var $version = '4.32';
+	var $version = '4.33';
 	var $errorTexts = array();
 	var $notificationTexts = array();
 	var $vcmsHostname;
