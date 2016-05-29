058aa9a6077d13766b5a226b868fa5efcaa587cb
uwol <u.wol@wwu.de>
2016-05-29 21:59:30 +0200

    version

--- lib/own/global.class.php
+++ lib/own/global.class.php
@@ -24,7 +24,7 @@ class LibGlobal{
 	var $page;
 	var $iid;
 	var $libInclude;
-	var $version = '4.48';
+	var $version = '4.49';
 	var $errorTexts = array();
 	var $notificationTexts = array();
 	var $vcmsHostname;
