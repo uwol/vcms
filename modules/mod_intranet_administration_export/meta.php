69a2f46e70a7d98b27d45e592080a7e5b968a5bb
uwol <u.wol@wwu.de>
2016-06-27 13:18:28 +0200

    condensed table

--- modules/mod_intranet_administration_export/meta.php
+++ modules/mod_intranet_administration_export/meta.php
@@ -1,6 +1,6 @@
 <?php
 $moduleName = "Intranet Administration für Export";
-$version = "2.09";
+$version = "2.10";
 $styleSheet = "";
 $installScript = "";
 $uninstallScript = "";
--- modules/mod_intranet_administration_export/scripts/export.php
+++ modules/mod_intranet_administration_export/scripts/export.php
@@ -42,7 +42,7 @@ $semester101zurueck = $libTime->getPreviousSemesterNameOfSemester($semester100zu
 <h1>Export</h1>
 <p>Das VCMS kann Datenbestände als CSV- und HTML-Tabellen exportieren. Die Dateien können in Word und LibreOffice/OpenOffice in der Serienbrieffunktion verwendet werden. Bitte behandle diese Dateien vertraulich, verschicke sie nicht per E-Mail und lösche sie nach der Verwendung.</p>
 
-<table class="table table-collapse">
+<table class="table table-condensed">
 	<tr>
 		<th colspan="2">Adressdaten</th>
 	</tr>
