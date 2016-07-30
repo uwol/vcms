cf520b207a78a5299b97b58067b9fb6bb3ec5c87
uwol <u.wol@wwu.de>
2016-07-30 11:57:32 +0200

    fix

--- modules/mod_internet_home/scripts/elements/facebook.php
+++ modules/mod_internet_home/scripts/elements/facebook.php
@@ -24,17 +24,17 @@ $fb_url = $libGenericStorage->loadValueInCurrentModule('fb_url');
 $showFbPagePlugin = $libGenericStorage->loadValueInCurrentModule('showFbPagePlugin');
 $fbPagePluginEnabled = $showFbPagePlugin && $fb_url != '';
 
-echo '<section class="facebook-box">';
-echo '<div class="container">';
-echo '<div class="row">';
-
 if($fbPagePluginEnabled){
+	echo '<section class="facebook-box">';
+	echo '<div class="container">';
+	echo '<div class="row">';
+
 	echo '<div style="max-width:500px" class="center-block">';
 	echo '<iframe src="https://www.facebook.com/plugins/page.php?href=' .urlencode($fb_url). '&tabs&width=340&height=154&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=true&appId" width="100%" height="154" class="facebookPagePlugin" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>';
 	echo '</div>';
-}
 
-echo '</div>';
-echo '</div>';
-echo '</section>';
+	echo '</div>';
+	echo '</div>';
+	echo '</section>';
+}
 ?>
