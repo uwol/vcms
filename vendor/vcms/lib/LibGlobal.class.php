782fd6270ba9deeebcc7ca3aa836547e7bc9a37f
uwol <u.wol@wwu.de>
2016-08-02 12:48:58 +0200

    version

--- vendor/vcms/lib/LibGlobal.class.php
+++ vendor/vcms/lib/LibGlobal.class.php
@@ -19,7 +19,7 @@ along with VCMS. If not, see <http://www.gnu.org/licenses/>.
 namespace vcms;
 
 class LibGlobal{
-	var $version = '6.53';
+	var $version = '6.54';
 
 	var $semester;
 	var $module;
