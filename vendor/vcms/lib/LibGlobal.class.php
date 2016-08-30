e0e11af41cc30ad4dbe3032235bd5c80754edecd
uwol <u.wol@wwu.de>
2016-08-30 18:41:49 +0200

    open sans

--- vendor/vcms/lib/LibGlobal.class.php
+++ vendor/vcms/lib/LibGlobal.class.php
@@ -19,7 +19,7 @@ along with VCMS. If not, see <http://www.gnu.org/licenses/>.
 namespace vcms;
 
 class LibGlobal{
-	var $version = '6.86';
+	var $version = '6.87';
 
 	var $semester;
 	var $module;
